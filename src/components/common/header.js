2cd3357f3eb5008e342ecd57388ac8ee0ed3ad02
littlewin <littlewin.wang@gmail.com>
2017-02-10 16:02:59 +0800

    增加List组件

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,6 +1,14 @@
 import React from 'react'
 require('styles/common/header.styl')
 
+import List from './list'
+
+const data = [
+  {'id':41,'thumbnail':'http://pic1.zhimg.com/921dd6c5d2477a0fc39fac3049e6f6ac.jpg','name':'16 年 9 月苹果秋季发布会'},
+  {'id':42,'name':'天宫二号发射','thumbnail':'http://pic1.zhimg.com/20382971f48562a73d99988cc1dd4e2c.jpg'},
+  {'thumbnail':'http://pic3.zhimg.com/1b9cea03d2f682c5c876f63b39b4797e.jpg','name':'2016 诺贝尔奖巡礼','id':44}
+]
+
 export default class Header extends React.Component {
   render() {
     return (
@@ -10,6 +18,7 @@ export default class Header extends React.Component {
         </div>
         <span>主题日报</span>
         <span>专栏总览</span>
+        <List items={data} />
       </div>
     );
   }
---
+++ src/components/common/list.js
@@ -0,0 +1,25 @@
+import React, { PropTypes } from 'react'
+require('styles/common/list.styl')
+
+import { imgProxy } from '../../api/utils'
+
+export default class List extends React.Component {
+  render() {
+    return (
+      <div className="list">
+        <ul>
+          {this.props.items.map((item) => (
+            <li title={item.name} key={item.id}>
+              <img src={imgProxy(item.thumbnail)} width="40" height="40" />
+              { item.name }
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+List.propTypes = {
+  items: PropTypes.array
+};
