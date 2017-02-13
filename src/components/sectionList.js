0c771c2a26206b833357793e440e85da528b525f
littlewin <littlewin.wang@gmail.com>
2017-02-13 15:05:36 +0800

    更新sectionList和topicList组件

---
+++ src/components/sectionList.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import API from '../api/index'
+import NewsItem from './common/newsItem'
+
+export default class SectionList extends React.Component {
+  constructor () {
+    super()
+    this.state = {sectionList: {'name': '', stories: [], timestamp: 0}}
+  }
+
+  componentDidMount () {
+    API.SectionIdResource(1)
+      .then(res => {
+        if (res.statusText === 'OK') {
+          this.setState({
+            sectionList: res.data
+          })
+        }
+      })
+  }
+
+  render () {
+    return (
+      <div className="sectionlist">
+        {this.state.sectionList.stories &&
+          <div className="section">
+            {this.state.sectionList.stories.map((news) => (
+              <NewsItem item={news} key={news.id}/>
+            ))}
+          </div>
+        }
+      </div>
+    )
+  }
+}
---
+++ src/components/topicList.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import API from '../api/index'
+import NewsItem from './common/newsItem'
+import { imgProxy } from '../api/utils'
+
+export default class TopicList extends React.Component {
+  constructor () {
+    super()
+    this.state = {topicPosts: {'description': '', image: '', stories: [], timestamp: 0}}
+  }
+
+  componentDidMount () {
+    API.TopicIdResource(3)
+      .then(res => {
+        if (res.statusText === 'OK') {
+          this.setState({
+            topicPosts: res.data
+          })
+        }
+      })
+  }
+
+  render () {
+    return (
+      <div className="topiclist">
+        <div className="img" style={{position: 'relative'}}>
+          <img src={imgProxy(this.state.topicPosts.image)} />
+          <p style={{position: 'absolute', left: '5%', bottom: '5%', textAlign: 'center', color: '#fff', cursor: 'pointer'}}>
+            {this.state.topicPosts.description}
+          </p>
+        </div>
+        {this.state.topicPosts.stories &&
+          <div>
+            {this.state.topicPosts.stories.map((news) => (
+              <NewsItem item={news} key={news.id}/>
+            ))}
+          </div>
+        }
+      </div>
+    )
+  }
+}
